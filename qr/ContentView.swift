2f8c066e2d929a329c73dd1bb1f927d4638d3407
xync <mixartemev@gmail.com>
2026-04-01 23:31:44 +0300

    Initial Commit

---
+++ qr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  qr
+//
+//  Created by Mike Artemiev on 01.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ qr/qrApp.swift
@@ -0,0 +1,17 @@
+//
+//  qrApp.swift
+//  qr
+//
+//  Created by Mike Artemiev on 01.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct qrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
